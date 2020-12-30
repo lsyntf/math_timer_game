6be09af032779d2bdd415902e332b7f91a39aa17
Leeam Ng Tang Fui <leeamng@gmail.com>
2020-12-30 02:02:55 -0500

    cindy recommend

--- display_questions.py
+++ display_questions.py
@@ -6,31 +6,32 @@ import random
 import random_eqn
 
 class display:
-    def __init__(self, rows, cols, time, start, diff, diff_inc, diff_change):
+    def __init__(self, rows, cols, times, question_size, level_up_thresh, start, diff, diff_inc, diff_change):
         self.rows = rows
         self.cols = cols
-        self.size = rows * cols
 
-        self.time_limit = time
-        self.count_limit = time
+        self.time_limit = times
+        self.time_left = times
+
+        self.question_size = question_size
 
         self.start = start
         self.diff = diff
         self.diff_inc = diff_inc
         self.diff_change = diff_change
 
-        self.default_question_cell = '           '
+        self.default_question_cell = ' ' * self.question_size
         self.default_answer_cell = -1
 
-        self.answers_correct = 0
-        self.was_correct = 0
+        self.num_correct_ans = 0
+        self.level_up_thresh = level_up_thresh
         self.attempts = 0
-        self.answered = False
+        self.is_answer_entered = False
 
-        self.compare_list = list(range(0, self.size))
+        self.available_indices = list(range(0, self.rows * self.cols))
 
-        self.table_answers = self.gen_a_table()
-        self.table_display = self.gen_q_table()
+        self.answer_table = self.gen_a_table()
+        self.question_table = self.gen_q_table()
 
         self.questions, self.answers = random_eqn.generate_question(self.start, self.diff, self.diff_inc)
 
@@ -45,7 +46,7 @@ class display:
             _ = os.system('clear') 
 
     def gen_a_table(self):
-        return [self.default_answer_cell] * self.size
+        return [self.default_answer_cell] * self.rows * self.cols
 
     def gen_q_table(self):
         data_table = []
@@ -56,92 +57,95 @@ class display:
             data_table.append(new)
         return data_table
 
-    def resize_question(self, q):
-        while len(q) < 11:
+    def resize_question(self, q): # stupid
+        while len(q) < self.question_size:
             q += ' '
         return q
 
     def countdown(self):
         print("Answer|Time")
-        while not self.answered and self.count_limit > 0.0:        
-            print(f"      |{round(self.count_limit)}", end = '\r')     
+        while not self.is_answer_entered and self.time_left > 0.0:  
+            # self.show_questions()
+            # print("Answer|Time")      
+            print(f"      |{int(self.time_left // 1)}", end = '\r')     
             for i in range(100):              
-                if self.answered:
+                if self.is_answer_entered:
                     break
                 time.sleep(0.01)                        
-                self.count_limit -= 0.01  
+                self.time_left -= 0.01  
 
     def difficulty(self):
-        correct_buf = self.answers_correct
-        if correct_buf > 0 and correct_buf % self.diff_change == 0 and self.was_correct != correct_buf:
+        correct_buf = self.num_correct_ans
+        if correct_buf > 0 and correct_buf % self.diff_change == 0 and self.time_left == self.time_limit:
             d = (correct_buf // self.diff_change)
             div = d // 2
             mod = d % 2
             if mod == 1:
                 self.diff += 1
-            if mod == 0 and self.time_limit >= 3:
+            elif mod == 0 and self.time_limit >= 3:
                 self.time_limit -= 1
-                self.count_limit -= 1
+                self.time_left -= 1
 
     def insert_val(self, question, answer):
-        if len(self.compare_list) == 0:
+        if len(self.available_indices) == 0:
             return
-        index_val = random.choice(self.compare_list)
-        self.table_display[index_val // self.cols][index_val % self.rows] = question
-        self.table_answers[index_val] = answer
-        self.compare_list.remove(index_val)
+        index_val = random.choice(self.available_indices)
+        self.question_table[index_val // self.cols][index_val % self.rows] = question
+        self.answer_table[index_val] = answer
+        self.available_indices.remove(index_val)
 
     def remove_val(self, val = -2):
-        answers = self.table_answers.count(val)
+        answers = self.answer_table.count(val)
         for i in range(answers):
-            index_val = self.table_answers.index(val)
-            self.table_display[index_val // self.cols].pop(index_val % self.rows)
-            self.table_display[index_val // self.cols].insert(index_val % self.rows, self.default_question_cell)
-            self.table_answers.remove(val)
-            self.table_answers.insert(index_val, self.default_answer_cell)
-            self.compare_list.append(index_val)
-            self.answers_correct += 1
-        if self.was_correct < self.answers_correct:
-            self.count_limit = self.time_limit
+            index_val = self.answer_table.index(val)
+            self.question_table[index_val // self.cols].pop(index_val % self.rows)
+            self.question_table[index_val // self.cols].insert(index_val % self.rows, self.default_question_cell)
+            self.answer_table.remove(val)
+            self.answer_table.insert(index_val, self.default_answer_cell)
+            self.available_indices.append(index_val)
+            self.num_correct_ans += 1
+            self.time_left = self.time_limit
         # print(table_a) 
 
-    def answer_question(self):    
+    def user_input(self):    
         # print("Answer|Time")
-        self.answered = False
+        self.is_answer_entered = False
         user_answer = input()
-        self.answered = True    
+        self.is_answer_entered = True    
         self.attempts += 1
         if user_answer != '' and user_answer.isnumeric():
             self.remove_val(int(user_answer))
         self.difficulty()
-        self.display_table()
-        return
+        self.show_questions()
+        self.answer_question()
 
-    def display_table(self):   
+    def show_questions(self):   
         # Each table cell must be 11 characters long
-        if len(self.compare_list) == 0:
+        if len(self.available_indices) == 0:
             print("You have reached the end! You lose!")
             os._exit(0)
-        if self.answers_correct == 100:
-            print("Congrats you have beat this level!")
-            return
+        if self.num_correct_ans == self.level_up_thresh:
+            print("Congrats, you beat the level!")
+            return True
+            
         self.clear()  
         self.questions, self.answers = random_eqn.generate_question(self.start, self.diff, self.diff_inc)
         self.insert_val(self.resize_question(self.questions), self.answers)
-        print_table = AsciiTable(self.table_display)
+        print_table = AsciiTable(self.question_table)
         print_table.inner_row_border = True
-        print(print_table.table)    
+        print(print_table.table) 
+        print(f"Correct Answers: {self.num_correct_ans} | Attempts: {self.attempts}")
+
+        # self.answer_question()   
         
-        before_ans = self.answers_correct 
-        self.was_correct = before_ans 
-        print(f"Correct Answers: {self.answers_correct} | Attempts: {self.attempts}")
-        answer_q = threading.Thread(target = self.answer_question)   
-        answer_q.start()
-        threading.Thread(target= self.countdown).start()
+    def answer_question(self):
+        before_ans = self.num_correct_ans 
+        threading.Thread(target = self.user_input).start() 
+        threading.Thread(target = self.countdown).start()
 
         time.sleep(self.time_limit)
-        # print(before_ans, answers_correct)
-        if before_ans == self.answers_correct:
-            # print(f'\nCorrect Answers: {self.answers_correct}')
+        # print(before_ans, num_correct_ans)
+        if before_ans == self.num_correct_ans:
+            # print(f'\nCorrect Answers: {self.num_correct_ans}')
+            print("Time's up!")
             os._exit(0)
-            return
--- main.py
+++ main.py
@@ -5,21 +5,29 @@ import time
 
 rows = 5
 cols = 5
-time_lim = 9.0
+time_lim = 5.0
+question_size = 11
+level_up_thresh = 100
 start_val = 4
 start_diff = 0
 diff_increment = 1
 diff_change = 10
 
-for i in range(3):
-    print(f"LEVEL {i + 1}")
-    for i in range(5, 0, -1):
-        print(i, end = "\r")
-        time.sleep(1)
+game = display_questions.display(rows, cols, time_lim, question_size, level_up_thresh, start_val, start_diff, diff_increment, diff_change)
+
+game.show_questions()
+game.answer_question()
+
+
+# for i in range(3):
+#     print(f"LEVEL {i + 1}")
+#     for i in range(5, 0, -1):
+#         print(i, end = "\r")
+#         time.sleep(1)
     
-    display_questions.display(rows, cols, time_lim, start_val, start_diff, diff_increment, diff_change).display_table()
-    rows -= 1
-    cols -= 1
-    time_lim -= 2
-    start_val += 2
+    
+#     rows -= 1
+#     cols -= 1
+#     time_lim -= 2
+#     start_val += 2
 
